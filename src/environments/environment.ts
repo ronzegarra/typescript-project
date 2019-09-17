71a351284c285bb582b68a8cce35a83c3a440906
ronald <zegarraronald@gmail.com>
2019-09-17 13:43:12 -0500

    agrego proyecto e interfaces

---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC7TnZeo-KubHUDz0p7SN_0Mh4geY_fvKM",
+    authDomain: "typescript-project-138e0.firebaseapp.com",
+    databaseURL: "https://typescript-project-138e0.firebaseio.com",
+    projectId: "typescript-project-138e0",
+    storageBucket: "",
+    messagingSenderId: "75932143455",
+    appId: "1:75932143455:web:2414fdc0aa1a9b7c5570ea"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
